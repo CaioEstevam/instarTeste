4197d2dc8c83d2ae72af9e51ee476d06d6774329
Caio Estevam <caioferreiramendes@gmail.com>
2020-05-17 23:29:24 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -3,18 +3,25 @@
 
 require_once ("config.php");
 
-$root = new Codigo();
+//Carrega apenas 1
+//$root = new Contatos();
+//$root->loadbyId(1);
+//echo $root;
 
-$root->loadbyId(2);
 
-echo $root;
+//carrega a lista dos contatos
+//$lista = contatos::getList();
+//echo json_encode($lista);
+
+//carrega uma lista de contatos buscando pelo nome
+$search = Contatos::search("Ic");
+echo json_encode($search);
+
+
 
 
 /*$sql = new sql();
-
 $contatos = $sql->select("SELECT * FROM tb_contatos");
-
-
 echo json_encode($contatos);*/
 
 
